d93ec97dc0d62a2cce2422c64ea3487f8d039dec
Auios <loneauios@gmail.com>
2020-11-01 13:28:36 -0600

    Update IQuadTreeObjectBounds.cs

--- Auios.QuadTree/IQuadTreeObjectBounds.cs
+++ Auios.QuadTree/IQuadTreeObjectBounds.cs
@@ -3,17 +3,11 @@
 
 namespace Auios.QuadTree
 {
-    /// <summary>Provides a set of methods for getting boundaries of the object.</summary>
-    /// <typeparam name="T">The type of object in the QuadTree.</typeparam>
     public interface IQuadTreeObjectBounds<in T>
     {
-        /// <summary>Gets let most edge of the object.</summary>
-        float GetLeft(T obj);
-        /// <summary>Gets the right most edge of the object.</summary>
-        float GetRight(T obj);
-        /// <summary>Gets the top most edge of the object.</summary>
         float GetTop(T obj);
-        /// <summary>Gets the bottom most edge of the object.</summary>
         float GetBottom(T obj);
+        float GetLeft(T obj);
+        float GetRight(T obj);
     }
 }
