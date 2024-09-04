129b82016cb62c8a8ace6508a6601a4885e6af34
Auios <loneauios@gmail.com>
2024-09-04 13:14:58 -0400

    Update to .NET 8

--- Auios.QuadTree/IQuadTreeObjectBounds.cs
+++ Auios.QuadTree/IQuadTreeObjectBounds.cs
@@ -1,10 +1,8 @@
-﻿// Copyright 2020 Connor Andrew Ngo
+﻿// Copyright 2024 Connor O'Connor
 // Licensed under the MIT License
 
-namespace Auios.QuadTree
-{
-    public interface IQuadTreeObjectBounds<in T>
-    {
+namespace Auios.QuadTree {
+    public interface IQuadTreeObjectBounds<in T> {
         float GetTop(T obj);
         float GetBottom(T obj);
         float GetLeft(T obj);
